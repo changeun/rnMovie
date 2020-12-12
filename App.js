e1f8aafd3093d12bee20b079135509293661a43a
changeun <jceun57@gmail.com>
2020-12-12 14:04:32 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import Stack from './navigation/Stack';
+
+const cacheImages = (images) => images.map(image => {
+  if (typeof image === "string") {
+    return Image.prefetch(image);
+  } else {
+    return Asset.fromModule(image).downloadAsync();
+  }
+});
+
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1601758174811-a04aad8f2245?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      require("./assets/splash.png")
+    ]);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ?
+    (<NavigationContainer>
+      <Stack />
+    </NavigationContainer>)
+    : (
+      <AppLoading
+        startAsync={loadAssets}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
